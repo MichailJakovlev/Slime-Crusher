7c9d0a8f98c9e520f44f0ad5e94270876c99e458
i7AXIS <618axis916@gmail.com>
2024-08-18 20:54:56 +0400

    bow for shooter

---
+++ Assets/Scripts/ShooterAttak.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShooterAttak : MonoBehaviour
+{
+    public SpawnSlimes _spawn;
+    public CharacterMovement _moveScript;
+    public Animator _characterAnim;
+    public float attackTime;
+
+    public void Start()
+    {
+        var _spawn = GetComponent<SpawnSlimes>();
+        var _moveScript = GetComponent<CharacterMovement>();
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Slime"))
+        {
+            if(Input.GetMouseButton(0))
+            {
+                StartCoroutine(Attack());
+                _spawn.SpawnOnce();
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        float startTime = Time.time;
+        _moveScript.isAttack = true;
+
+        while (Time.time < startTime + attackTime)
+        {
+            _characterAnim.Play("Shooter.BowShot");
+            yield return null;
+        }
+        _moveScript.isAttack = false;
+    }
+}
