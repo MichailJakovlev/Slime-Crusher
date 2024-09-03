60894e75b9ac921602f1f7936e9c6073440ce410
i7AXIS <618axis916@gmail.com>
2024-09-03 20:02:17 +0400

    add reward controller

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -9,23 +9,32 @@ public class CharacterSelection : MonoBehaviour
   [SerializeField] private Button nextButton;
   private int currentCharacter;
 
+  public RewardController _rewardController;
+
+  void Start()
+  {
+    ChangeCharacter(0);
+  }
+
   private void Awake()
   {
     SelectCharacter(0);
   }
-    private void SelectCharacter(int _index)
-    {
-      previousButton.interactable = (_index != 0);
-      nextButton.interactable = (_index != transform.childCount - 1);
-      for (int i = 0; i < transform.childCount; i++)
-      {
-        transform.GetChild(i).gameObject.SetActive(i == _index);
-      }
-    }
 
-    public void ChangeCharacter(int _change)
+  private void SelectCharacter(int _index)
+  {
+    previousButton.interactable = (_index != 0);
+    nextButton.interactable = (_index != transform.childCount - 1);
+    for (int i = 0; i < transform.childCount; i++)
     {
-      currentCharacter += _change;
-      SelectCharacter(currentCharacter);
+      transform.GetChild(i).gameObject.SetActive(i == _index);
     }
+  }
+
+  public void ChangeCharacter(int _change)
+  {
+    currentCharacter += _change;
+    _rewardController.RewordHandler(currentCharacter);
+    SelectCharacter(currentCharacter);
+  }
 }
